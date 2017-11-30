f8e651fe31eebca2aae255525c7a9825ec89da99
Bartłomiej Majerski <bartlomiej.majerski.96@gmail.com>
2017-11-30 15:05:28 +0100

    first classes

---
+++ Edge.py
@@ -0,0 +1,7 @@
+class Edge:
+    "klasa krawędzi"
+    def __init__(self, id, id_to, id_from, id_jezdni):
+        self.id = []
+        self.id_to = []
+        self.id_from = []
+        self.id_jezdni = []
---
+++ Node.py
@@ -0,0 +1,6 @@
+class Node:
+    "klasa wierzchołków"
+    def __init__(self, id, x, y):
+        self.id = []
+        self.x = []
+        self.y = []
