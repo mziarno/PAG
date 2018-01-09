6fc9fd6a155a59093672a11d76cc3962e171c525
mziarno <marcelinaa.ziarno@gmail.com>
2018-01-09 14:43:07 +0100

    distance

--- Edge.py
+++ Edge.py
@@ -1,5 +1,6 @@
 from tools import createEdgeId
 from tools import convertVelocity
+from tools import findDistance
 
 
 class Edge:
@@ -10,8 +11,7 @@ class Edge:
         self.id = createEdgeId(s_node, e_node)
         self.dist = self.distance(nodes)
 
-        #self.time = self.timeOfDist()
-
+        print self.dist
 
     def checkLimit(self, road_class):
         road_classes = {"A": 120, "S": 100, "GP": 90, "G": 90, "Z": 60, "L": 50, "D": 30, "I": 30}
@@ -24,6 +24,10 @@ class Edge:
 
         x2 = nodes[self.e].x
         y2 = nodes[self.e].y
+
+        return findDistance(x1, y1, x2, y2)
+
+
 
 
 
